8e2935a7e3606d28d089b4918ba269e84c0f3ea9
Vishwas <vishwasbvbharathesh@gmail.com>
2026-06-29 23:14:46 +0530

    vis

--- Backend/src/main/java/com/apex/routine/ApexRoutineApplication.java
+++ Backend/src/main/java/com/apex/routine/ApexRoutineApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.apex.routine;
+package com.apex.routine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
